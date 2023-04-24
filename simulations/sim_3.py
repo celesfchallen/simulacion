9bac44a2d5351327caac1af7ce07bbc690ef5207
Celeste Fernandez Challen <celestefchallen@gmail.com>
2023-04-24 20:23:14 -0300

    Adds NEXT_TA variable

--- simulations/sim_3.py
+++ simulations/sim_3.py
@@ -12,6 +12,7 @@ T: Time
 TPLL: Time
 TPS: Time
 TE: Time
+NEXT_TA: Time
 Calculated_TA: list[Time]
 
 
@@ -30,7 +31,7 @@ def run_simulation_3(simulation_duration):
 
 
 def set_initial_conditions():
-    global NS, CLL, T, TPLL, TPS, TA, IA, TE, Calculated_TA
+    global NS, CLL, T, TPLL, TPS, TA, IA, TE, Calculated_TA, NEXT_TA
     NS = 0
     CLL = 0
     T = Time(0, 0)
@@ -40,6 +41,7 @@ def set_initial_conditions():
     TA = (10, 20)
     TE = Time(0, 0)
     Calculated_TA = []
+    NEXT_TA = Time(0, 0)
 
 
 def run_simulation():
@@ -49,25 +51,31 @@ def run_simulation():
         leave_routine()
 
 
-def set_TPS():
-    global TPS
-    TPS = T + Calculated_TA[0]
-
-
 def arrive_routine():
     global T
     T = TPLL
     calculate_TPLL()
     regrets = regret_routine()
-    calculate_TA()
+    calculate_NEXT_TA()
     if not regrets:
         increment_NS()
         increment_CLL()
+        set_Next_TA()
         print("Input: ", T)
         if NS == 1:
             set_TPS()
+
+
+def leave_routine():
+    global T, TPS
+    T = TPS
+    decrement_NS()
+    update_TE()
+    if NS > 0:
+        set_TPS()
     else:
-        update_calculated_TA()
+        TPS = high_value_time()
+    print("Output: ", T)
 
 
 def regret_routine():
@@ -85,24 +93,9 @@ def regret_routine():
         return True
 
 
-def update_calculated_TA():
-    global TE
-    TE = TE - Calculated_TA[0]
-    Calculated_TA.pop(0)
-    print(Calculated_TA)
-    print(TE)
-
-
-def leave_routine():
-    global T, TPS
-    T = TPS
-    decrement_NS()
-    update_calculated_TA()
-    if NS > 0:
-        set_TPS()
-    else:
-        TPS = high_value_time()
-    print("Output: ", T)
+def set_TPS():
+    global TPS
+    TPS = T + Calculated_TA[0]
 
 
 def empty_system():
@@ -112,18 +105,28 @@ def empty_system():
         run_simulation()
 
 
+def update_TE():
+    global TE
+    TE = TE - Calculated_TA[0]
+    Calculated_TA.pop(0)
+
+
+def set_Next_TA():
+    global TE
+    Calculated_TA.append(NEXT_TA)
+    TE = TE + NEXT_TA
+
+
 def calculate_TPLL():
     global TPLL
     random_number = random.randint(IA[0], IA[1])
     TPLL = TPLL + build_from_minutes(random_number)
 
 
-def calculate_TA():
-    global TE
+def calculate_NEXT_TA():
+    global NEXT_TA
     random_number = random.randint(TA[0], TA[1])
-    new_TA = build_from_minutes(random_number)
-    Calculated_TA.append(new_TA)
-    TE = TE + new_TA
+    NEXT_TA = build_from_minutes(random_number)
 
 
 def decrement_NS():
